a34f30c4a76ef573a8aaaa21d088d91b07255a74
micahwierenga <wierenga.micah@gmail.com>
2020-10-21 15:42:38 -0600

    Adds Edit link

--- src/components/Games/GameCard.js
+++ src/components/Games/GameCard.js
@@ -4,15 +4,18 @@ import { Link } from 'react-router-dom';
 const GameCard = (props) => {
     const { title, publisher, coverArtUrl, _id } = props.game;
     return (
-        <Link to={`/games/${_id}`}>
-            <div className='game-card'>
-                <div className='image-wrapper'>
-                    <img src={coverArtUrl} alt={title} />
+        <>
+            <Link to={`/games/${_id}`}>
+                <div className='game-card'>
+                    <div className='image-wrapper'>
+                        <img src={coverArtUrl} alt={title} />
+                    </div>
+                    <h3>{title}</h3>
+                    <p>By: {publisher}</p>
                 </div>
-                <h3>{title}</h3>
-                <p>By: {publisher}</p>
-            </div>
-        </Link>
+            </Link>
+            <Link to={`/games/${_id}/edit`} style={{color: 'black'}}>Edit</Link>
+        </>
     )
 }
 
