05d84dee5e6e3002677427a39f23a34043dc6470
Dalton <daltonvaren@gmail.com>
2020-10-22 09:16:16 -0700

    refactors gamelist into hooks

--- src/pages/GameList.js
+++ src/pages/GameList.js
@@ -1,30 +1,52 @@
-import React from 'react';
-import Games from '../components/Games/Games';
-import GameModel from '../models/GameModel';
+import React, { useState, useEffect } from "react";
+import Games from "../components/Games/Games";
+import GameModel from "../models/GameModel";
 
-class GameList extends React.Component {
-    state = {
-        games: [],
-    }
+// class GameList extends React.Component {
+//     state = {
+//         games: [],
+//     }
 
-    componentDidMount() {
-        this.fetchGames();
-    }
+//     componentDidMount() {
+//         this.fetchGames();
+//     }
 
-    fetchGames = () => {
-        GameModel.all()
-            .then(json => {
-                this.setState({
-                    games: json.games
-                })
-            })
-    }
+//     fetchGames = () => {
+//         GameModel.all()
+//             .then(json => {
+//                 this.setState({
+//                     games: json.games
+//                 })
+//             })
+//     }
 
-    render() {
-        return (
-            <Games data={this.state.games} />
-        )
-    }
+//     render() {
+//         return (
+//             <Games data={this.state.games} />
+//         )
+//     }
+// }
+
+function GameList(props) {
+  const [games, setGames] = useState([]);
+
+  useEffect(function () {
+    console.log("useEffect was called on mount");
+    fetchGames();
+  }, []);
+
+  function fetchGames() {
+    GameModel.all().then((json) => {
+      setGames(json.games);
+    });
+  }
+
+  return (
+    <div>
+      <h1>All Games</h1>
+      <Games data={games} />
+    </div>
+  );
 }
 
 export default GameList;
