449d8371c15071656f08401913158a670f72723a
Dalton <daltonvaren@gmail.com>
2020-10-22 14:15:59 -0700

    adds base recoil setup and user in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import './Header.css';
+// import to use the recoil state
+import { useRecoilState } from "recoil";
+// import the atom that controls the global state
+import { userState } from "../../recoil/atoms";
+
+import "./Header.css";
 
 const Header = (props) => {
-    return (
-        <header>
-            <div className='logo'>
-                <NavLink to='/'>Games!</NavLink>
-            </div>
-            <div className='links'>
-                <ul>
-                    <li>
-                        <NavLink to='/games'>All Games</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/games/new'>Add New Game</NavLink>
-                    </li>
-                </ul>
-            </div>
-        </header>
-    )
-}
+  const [user, setUser] = useRecoilState(userState);
+
+  function login() {
+    const dbUser = {
+      username: "Test user",
+      avatar:
+        "https://gamespot1.cbsistatic.com/uploads/scale_landscape/1587/15875866/3660435-avatar.jpg",
+    };
+
+    setUser(dbUser);
+  }
+
+  function logout() {
+    setUser(null);
+  }
+
+  return (
+    <header>
+      <div className='logo'>
+        <NavLink to='/'>Games!</NavLink>
+      </div>
+      <div className='links'>
+        <ul>
+          {user ? (
+            <>
+              <li>{user.username}</li>
+              <li>
+                <NavLink to={"/games"}>All Games</NavLink>
+              </li>
+              <li>
+                <NavLink to={"/games/new"}>Add New Game</NavLink>
+              </li>
+              <li className='btn' onClick={logout}>
+                Log Out
+              </li>
+            </>
+          ) : (
+            <li className='btn' onClick={login}>
+              Log in
+            </li>
+          )}
+        </ul>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import "./index.css";
+import App from "./App";
 
 ReactDOM.render(
-  // StrictMode is a tool for highlighting potential 
-  // problems in an application. Like Fragment, 
-  // StrictMode does not render any visible UI. 
-  // It activates additional checks and warnings for 
+  // StrictMode is a tool for highlighting potential
+  // problems in an application. Like Fragment,
+  // StrictMode does not render any visible UI.
+  // It activates additional checks and warnings for
   // its descendants.
   <React.StrictMode>
-    <Router>  
-      <App />
-    </Router>
+    <RecoilRoot>
+      <Router>
+        <App />
+      </Router>
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
