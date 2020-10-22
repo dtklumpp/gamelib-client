57795c77bfba1d6e52cede232ce8e976cb7b0fec
Dalton <daltonvaren@gmail.com>
2020-10-22 10:12:52 -0700

    refactors show and new into hooks

--- src/pages/GameShow.js
+++ src/pages/GameShow.js
@@ -1,32 +1,21 @@
-import React from 'react';
-import GameModel from '../models/GameModel';
-import GameCard from '../components/Games/GameCard';
+import React, { useState, useEffect } from "react";
+import GameModel from "../models/GameModel";
+import GameCard from "../components/Games/GameCard";
 
-class GameShow extends React.Component {
-    state = {
-        game: null,
-    }
+function GameShow(props) {
+  const [game, setGame] = useState(null);
 
-    componentDidMount() {
-        this.fetchGame();
-    }
+  useEffect(function () {
+    fetchGame();
+  }, []);
 
-    fetchGame = () => {
-        GameModel.show(this.props.match.params.id)
-            .then(json => {
-                this.setState({
-                    game: json.game
-                })
-            })
-    }
+  function fetchGame() {
+    GameModel.show(props.match.params.id).then((data) => {
+      setGame(data.game);
+    });
+  }
 
-    render() {
-        return (
-            this.state.game ?
-                <GameCard game={this.state.game} /> :
-                <h3>Loading...</h3>
-        )
-    }
+  return game ? <GameCard game={game} /> : <h3>Loading...</h3>;
 }
 
 export default GameShow;
--- src/pages/NewGame.js
+++ src/pages/NewGame.js
@@ -1,80 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import GameModel from "../models/GameModel";
 
-class NewGame extends React.Component {
-  state = {
-    title: "",
-    publisher: "",
-    coverArtUrl: "",
-    completed: false,
-  };
+function NewGame(props) {
+  const [title, setTitle] = useState("");
+  const [publisher, setPublisher] = useState("");
+  const [coverArtUrl, setCoverArtUrl] = useState("");
+  const [completed, setCompleted] = useState(false);
 
-  handleSubmit = (event) => {
+  function handleSubmit(event) {
     event.preventDefault();
 
-    GameModel.create(this.state).then((json) => {
-      this.props.history.push("/games");
-    });
-  };
-
-  handleChange = (event) => {
-    if (event.target.type !== "text") {
-      this.setState((prevState) => ({
-        completed: !prevState.completed,
-      }));
-    } else {
-      this.setState({
-        [event.target.name]: event.target.value,
-      });
-    }
-  };
-
-  render() {
-    return (
-      <div>
-        <h2>New Game</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div className='form-input'>
-            <label htmlFor='title'>Title</label>
-            <input
-              type='text'
-              name='title'
-              onChange={this.handleChange}
-              value={this.state.title}
-            />
-          </div>
-          <div className='form-input'>
-            <label htmlFor='publisher'>Publisher</label>
-            <input
-              type='text'
-              name='publisher'
-              onChange={this.handleChange}
-              value={this.state.publisher}
-            />
-          </div>
-          <div className='form-input'>
-            <label htmlFor='coverArtUrl'>Image URL</label>
-            <input
-              type='text'
-              name='coverArtUrl'
-              onChange={this.handleChange}
-              value={this.state.coverArtUrl}
-            />
-          </div>
-          <div className='form-input'>
-            <label htmlFor='completed'>Completed</label>
-            <input
-              type='checkbox'
-              name='completed'
-              onChange={this.handleChange}
-              value={this.state.completed}
-            />
-          </div>
-          <input type='submit' value='Save Game' />
-        </form>
-      </div>
+    GameModel.create({ title, publisher, coverArtUrl, completed }).then(
+      (data) => {
+        props.history.push("/games");
+      }
     );
   }
+
+  return (
+    <div>
+      <h2>New Game</h2>
+      <form onSubmit={handleSubmit}>
+        <div className='form-input'>
+          <label htmlFor='title'>Title</label>
+          <input
+            type='text'
+            name='title'
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </div>
+        <div className='form-input'>
+          <label htmlFor='publisher'>Publisher</label>
+          <input
+            type='text'
+            name='publisher'
+            onChange={(e) => setPublisher(e.target.value)}
+            value={publisher}
+          />
+        </div>
+        <div className='form-input'>
+          <label htmlFor='coverArtUrl'>Image URL</label>
+          <input
+            type='text'
+            name='coverArtUrl'
+            onChange={(e) => setCoverArtUrl(e.target.value)}
+            value={coverArtUrl}
+          />
+        </div>
+        <div className='form-input'>
+          <label htmlFor='completed'>Completed</label>
+          <input
+            type='checkbox'
+            id='completed'
+            checked={completed}
+            onChange={(e) => setCompleted(!completed)}
+          />
+        </div>
+
+        <input type='submit' value='Save!' />
+      </form>
+    </div>
+  );
 }
 
 export default NewGame;
